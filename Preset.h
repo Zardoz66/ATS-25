4887c0857176e40701cd1133702af21f1c4ecc3f
Zardoz66 <44716085+Zardoz66@users.noreply.github.com>
2022-08-10 22:10:12 -0500

    Add files via upload

---
+++ Preset.h
@@ -0,0 +1,127 @@
+//======================================================= Presets Groups    ================================
+Group group[] PROGMEM = {
+//  733,    "city 1",
+//  737,    "city 2",
+//  762,    "city 3",
+//  769,    "city 4",
+//  771,    "city 5",
+//  776,    "city 6",
+    777,    "Clearlake CA",
+//  778,    "city 8",
+};
+//======================================================= END Presets Groups     ===========================
+
+//======================================================= Memory     =======================================
+Memory memory[] PROGMEM = {
+
+  88.1,   "KWTF",     "777",  // 
+  88.5,   "KPFZ",     "777",  // 
+  88.7,   "KORB",     "777",  // 
+  89.1,   "KBBF",     "777",  // 
+  89.5,   "KPRA",     "777",  // 
+  89.9,   "KOSC",     "777",  // 
+  91.1,   "KRCG",     "777",  // 
+  91.3,   "KSVY",     "777",  // 
+  91.5,   "KZYZ",     "777",  // 
+  91.9,   "KLVR",     "777",  // 
+  92.9,   "KFGY",     "777",  // 
+  93.5,   "KMKX",     "777",  // 
+  93.7,   "KBBL",     "777",  // 
+  94.5,   "KWNE",     "777",  // 
+  95.3,   "KUIC",     "777",  // 
+  95.9,   "KRSH",     "777",  // 
+  96.7,   "KNOB",     "777",  // 
+  97.1,   "KULV",     "777",  // 
+  97.7,   "KVRV",     "777",  //
+  97.9,   "KLMG",     "777",  // 
+  98.3,   "KXBX",     "777",  // 
+  98.7,   "KXTS",     "777",  // 
+  99.1,   "KARA",     "777",  // 
+  99.5,   "KNTI",     "777",  // 
+  99.9,   "KRCX",     "777",  // 
+  100.1,  "KZXT",     "777",  // 
+  100.3,  "KMOB-LP",  "777",  // 
+  100.5,  "KTDE",     "777",  // 
+  100.9,  "KSXY",     "777",  // 
+  101.7,  "KHTH",     "777",  // 
+  102.3,  "KYOE",     "777",  // 
+  102.5,  "KSFM",     "777",  // 
+  102.7,  "KWVF",     "777",  // 
+  103.1,  "KKCY",     "777",  // 
+  103.3,  "KUKI",     "777",  // 
+  104.1,  "KJOR",     "777",  // 
+  104.3,  "KXSE",     "777",  // 
+  104.9,  "KRCB",     "777",  // 
+  105.5,  "KSAC",     "777",  // 
+  105.9,  "KQPM",     "777",  // 
+  106.3,  "KPQW",     "777",  // 
+  107.1,  "KSRT",     "777",  // 
+  107.5,  "KZSZ",     "777",  // 
+  
+ //  153,  "ROMANIA Actualitati", "ALL",  //
+ //  162,  "FRANCIA Intern.",     "ALL",  //
+ //  171,  "MAROCCO Medi 1",      "ALL",  //
+ //  183,  "FRANCIA Europe 1",    "ALL",  //
+ //  198,  "BBC Radio 4",         "ALL",  //
+ //  207,  "Deutschland Radio",   "ALL",  //
+ //  225,  "POLONIA Radio",       "ALL",  //
+ //  234,  "RTL Luxenbourg",      "ALL",  //
+ //  252,  "ALGERIA Chaine 3",    "ALL",  //
+ //  270,  "CRO Czech.Repubbl.",  "ALL",  //
+
+
+ //  531,  "ALGERIA JIL Radio",         "777",  //
+ //  540,  "UNGHERIA Radio",            "777",  //
+ //  549,  "Radio Capodistria",         "777",  //
+ //  558,  "ROMANIA Actualitati",       "777",  //
+ //  576,  "BULGARIA National",         "777",  //
+ //  585,  "RADIO VATICANA",            "777",  //
+ //  603,  "FRANCE Info",               "777",  //
+ //  621,  "ERTU Egitto",               "777",  //
+ //  630,  "RTT TUNISIA",               "777",  //
+ //  657,  "RAI Coltano (PI)",          "777",  //
+ //  666,  "RDP Portogallo",            "777",  //
+ //  675,  "SAWT LIBIA Bengasi",        "777",  //
+ //  693,  "BBC Radio 5",               "777",  //
+ //  702,  "Radio CINA Int.",           "777",  //
+ //  711,  "Cadena SPAGNA",             "777",  //
+ //  720,  "BBC R.4",                   "777",  //
+ //  729,  "ERA GRECIA",                "777",  //
+ //  738,  "Radio Naz. Spagna",         "777",  //
+ //  747,  "BULGARIA Radio",            "777",  //
+ //  756,  "ROMANIA Actualitati",       "777",  //
+ //  783,  "MDR GERMANIA",              "777",  //
+ //  792,  "ERA GRECIA",                "777",  //
+ //  810,  "R.Skopye MACEDONIA",        "777",  //
+ //  864,  "ERTU EGITTO",               "777",  //
+ //  873,  "R.Nacional Espana",         "777",  //
+ //  882,  "CRNA NONTENEGRO",           "777",  //
+ //  900,  "RAI Siziano (PV)",          "777",  //
+ //  936,  "RAI Campallo (VE)",         "777",  //
+ //  981,  "RAI Monteradio (TS)",       "777",  //
+ //  999,  "RAI Volpiano (TO)",         "777",  //
+ //  1062,  "RAI 2 Catania",            "777",  //
+ //  1080,  "R.Cadena Spagna",          "777",  //
+ //  1098,  "Radio Patria Slovacchia",  "777",  //
+ //  1107,  "RAI 1 Roma",               "777",  //
+ //  1116,  "RAI MontePellegrino (PA)", "777",  //
+ //  1170,  "Radio Capodistria",        "777",  //
+ //  1179,  "ROMANIA Radio",            "777",  // 
+ //  1215,  "R.Cina Int.",              "777",  //
+ //  1233,  "R.Dechovka Rep.Ceca",      "777",  //
+ //  1251,  "SAWT Libia",               "777",  //
+ //  1296,  "R.Cadena Spagna",          "777",  //
+ //  1413,  "Voice of RUSSIA ",         "777",  //
+ //  1431,  "RAI Foggia (FG)",          "777",  //
+ //  1449,  "RAI Belluno",              "777",  //
+ //  1458,  "R.Tirana Albania",         "777",  //
+ //  1521,  "BSKSA Arabia Saudita",     "777",  //
+ //  1575,  "RAI Portofino (GE)",       "777",  //
+ //  1584,  "R.StudioX Toscana",        "777",  //
+ //  1602,  "R.Cartagena Spagna",       "777",  //
+
+   
+ //  7260,  "Asiatic Radio",       "ALL",  //
+  27185,  "CB Ch.19",         "ALL",  //
+};
+//======================================================= END Memory     ===================================
